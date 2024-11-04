372cd5034efe73292b29a877977a32cfae6b39d2
김도형 <mrdos@naver.com>
2024-11-04 15:57:33 +0900

    for 예제 추

--- src/com/javaex/basic/flow/LoopEx.java
+++ src/com/javaex/basic/flow/LoopEx.java
@@ -6,7 +6,10 @@ public class LoopEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		whileEx02();
+//		whileEx01();
+//		whileEx02();
+//		doWhileEx();
+		forEx();
 	}
 
 	private static void whileEx01() {
@@ -21,7 +24,6 @@ public class LoopEx {
 		}
 	}
 	
-
 	private static void whileEx02() {
 		System.out.println("단을 입력해주세요.");
 		System.out.print("단: ");
@@ -35,6 +37,38 @@ public class LoopEx {
 			System.out.println(danNum + " X " + i + " = " + danNum*i);
 			
 			i += 1;
+		}
+		
+		scanner.close();
+	}
+	
+	private static void doWhileEx() {
+		Scanner scanner = new Scanner(System.in);
+		
+		int sum = 0;
+		int inputNum;
+		
+		System.out.println("숫자를 입력하세요. [0이면 종료]");
+		do {
+			inputNum = scanner.nextInt();
+			sum += inputNum;
+			
+			System.out.println("합계: " + sum);
+		} while(inputNum != 0);
+		
+		System.out.println("종료");
+		scanner.close();
+	}
+	
+	private static void forEx() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("단을 입력해주세요.");
+		System.out.print("단: ");
+		
+		int danNum = scanner.nextInt();
+		
+		for(int i = 1; i < 10; ++i) {
+			System.out.println(danNum + " X " + i + " = " + danNum*i);
 		}
 		
 		scanner.close();
