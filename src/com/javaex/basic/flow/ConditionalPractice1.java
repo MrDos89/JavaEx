f2b93ce298c915b80e55e07f1560baffb8a0da6c
김도형 <mrdos@naver.com>
2024-11-04 12:15:55 +0900

    [도형]switch 예제 추가

--- src/com/javaex/basic/flow/ConditionalPractice1.java
+++ src/com/javaex/basic/flow/ConditionalPractice1.java
@@ -2,15 +2,19 @@ package com.javaex.basic.flow;
 
 import java.util.Scanner;
 
-public class ConditionalPractice1 {
+public class ConditionalPractice1 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-		ifPractice01();
+//		ifPractice01();
+		switchEx();
 		
 	}
 	
-	private static void ifPractice01() {
+	private static void ifPractice01() 
+	{
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("과목을 선택하세요.");
@@ -37,6 +41,37 @@ public class ConditionalPractice1 {
 		else
 		{
 			System.out.print("상담원에게 문의하세요.");
+		}
+		
+		scanner.close();
+	}
+	
+	private static void switchEx() 
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("과목을 선택하세요.");
+		System.out.println("(1.자바 2.C++ 3.C 4.파이썬)");
+		System.out.print("과목 번호: ");
+		int classNum = scanner.nextInt();
+		
+		switch(classNum)
+		{
+			case 1:
+				System.out.print("R101호 입니다.");
+				break;
+			case 2:
+				System.out.print("R202호 입니다.");
+				break;
+			case 3:
+				System.out.print("R303호 입니다.");
+				break;
+			case 4:
+				System.out.print("R404호 입니다.");
+				break;
+			default:
+				System.out.print("상담원에게 문의하세요.");
+				break;
 		}
 		
 		scanner.close();
