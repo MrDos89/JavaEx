08b059459395bc995149495360b4dd43217029c8
김도형 <mrdos@naver.com>
2024-11-06 17:50:49 +0900

    메소드 예제 추

---
+++ src/com/javaex/oop/MethodsEx.java
@@ -0,0 +1,43 @@
+package com.javaex.oop;
+
+public class MethodsEx {
+
+	// 메서드 작성 연습
+	// 매개 변수 -> 입력, 리턴 -> 출력
+	public static void main(String[] args) {
+		// 매개변수 x, 리턴 x
+		printMessage();
+		// 매개변수 x, 리턴 o
+		System.out.printf("Message : " + getMessage() + "\n");
+		// 매개변수 o, 리턴 x
+		printSum(3, 5);
+		// 매개변수 o, 리턴 o
+		System.out.printf("%f", getSum(3, 5));
+	}
+	
+	// 매개변수 x -> 파라미터 없음
+	// 리턴 x -> void
+	private static void printMessage() {
+		System.out.println("매개변수 없고, 리턴도 없음");
+	}
+
+	// 매개변수 x -> 파라미터 없음
+	// 리턴 o -> 리턴할 데이터의 타입
+	private static String getMessage() {
+		// return으로 데이터 반환
+		return "입력은 없지만, 출력은 있는 메서드";
+	}
+	
+	// 매개변수 o -> 파라미터 있음
+	// 리턴 x -> void
+	private static void printSum(double num1, double num2) {
+		System.out.println(num1 + num2);
+	}
+
+	// 매개변수 o -> 파라미터 있음
+	// 리턴 o -> 리턴할 데이터의 타입 명시
+	private static double getSum(double num1, double num2) {
+		return num1 + num2;
+	}
+	
+}
